99b38e1abad038a81f49c48a1d50c7d9f8d0caed
Igor Kuzmin <kialkuz@yandex.ru>
2026-03-22 18:11:16 +0300

    fix

--- internal/infrastructure/repository/db/repository.go
+++ internal/infrastructure/repository/db/repository.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/lib/pq"
 )
@@ -10,6 +11,20 @@ func NewStorage(dbType string, databaseURI string) (MetricsRepository, error) {
 	db, err := sql.Open(dbType, databaseURI)
 	if err != nil {
 		return nil, err
+	}
+
+	_, err = db.Exec(`
+		CREATE SEQUENCE users_id_seq;
+		CREATE TABLE IF NOT EXISTS public.metrics (
+			id int4 DEFAULT nextval('users_id_seq'::regclass) NOT NULL,
+			"type" varchar(50) NOT NULL,
+			"name" varchar(50) NOT NULL,
+			value float8 NOT NULL,
+			CONSTRAINT users_pkey PRIMARY KEY (id)
+		);
+	`)
+	if err != nil {
+		log.Println(err)
 	}
 
 	return &MemStorage{db: db}, nil
